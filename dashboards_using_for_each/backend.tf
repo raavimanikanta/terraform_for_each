75fd10e8c428c6a22a1f4f22293e71b487f328ea
raavimanikanta <92731330+raavimanikanta@users.noreply.github.com>
2024-05-21 20:42:38 +0530

    Update backend.tf

--- dashboards_using_for_each/backend.tf
+++ dashboards_using_for_each/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         	   = "mani-terraform-pipeline"
-    key              	   = "state/terraform.tfstate"
-    region         	   = "us-east-1"
-#     encrypt        	   = true
-  #  dynamodb_table = "mcd_nr_pipeline_table"
-  }
-}
+
